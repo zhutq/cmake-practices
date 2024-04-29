58ea9c46ecce49a9df83f919b243bc78312a839a
Tianqian Zhu <zhutianqian@gmail.com>
2024-04-29 17:17:38 +0800

    Better definition for dllexport

--- shared-library/echo/exports.h
+++ shared-library/echo/exports.h
@@ -1,10 +1,14 @@
 #ifndef EXPORTS_H
 #define EXPORTS_H
 
+#if defined(_WIN32)
 #ifdef ECHO_COMPILE_LIBRARY
-   #define ECHO_API __declspec(dllexport)
+#define ECHO_API __declspec(dllexport)
 #else
-   #define ECHO_API __declspec(dllimport)
+#define ECHO_API __declspec(dllimport)
 #endif
+#else
+#define ECHO_API __attribute__((visibility("default")))
+#endif // _WIN32
 
 #endif /* EXPORTS_H */
